b3ff563ed5f87a0f74565c12901463cd21d234b8
Thiago Cabral <thiagoo.cabral@gmail.com>
2024-02-01 20:22:40 -0300

    fix lib folder and visual studio launch config

--- src/view/TabsController.java
+++ src/view/TabsController.java
@@ -290,7 +290,7 @@ public class TabsController {
         applyMacro.setOnAction(this::applyMacro);
 
         LocalDate hoje = LocalDate.now();
-        boolean auxtmp = hoje.isBefore(LocalDate.of(2018, Month.JUNE, 30));
+        boolean auxtmp = hoje.isBefore(LocalDate.of(2025, Month.JUNE, 30));
         System.out.println(auxtmp);
         if (!auxtmp) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
